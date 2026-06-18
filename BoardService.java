e51f93738853622e7d1e1fb9d798c917fe29814b
iingjeong <silvmin42@gmail.com>
2026-06-18 17:33:36 +0900

    fix:BoardService 전체 코드 복구

--- BoardService.java
+++ BoardService.java
@@ -1,3 +1,12 @@
+import java.util.*;
+public class BoardService {
+    private List<Board> boards = new ArrayList<>();
+    public List<Board> findAll() { return boards; }
+    public Board findById(int id) {
+        return boards.stream()
+                     .filter(b -> b.getId() == id)
+                     .findFirst().orElse(null);
+    }
     // feature/B: 삭제 기능 추가
     public boolean delete(int id) {
         Board board = findById(id);
@@ -14,5 +23,5 @@
         board.setContent(newContent);
         return board;
     }
-
+}
 //B계정에서 추가함
